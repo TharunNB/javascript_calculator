66e7febe1035e2f9e1800708c60626c7158651a1
TharunNB <tharunbhoopathy@gmail.com>
2023-10-21 20:15:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [result,setResult] = useState("");
+
+  const ops = ['/','*','+','-','.'];
+
+  const updateCalc = (value) =>{
+      if( 
+        (ops.includes(value) && calc === '') ||
+        (ops.includes(value) && ops.includes(calc.slice(-1)))
+      ){
+        return;
+      }
+      setCalc(calc+value);
+
+      if (!ops.includes(value)){
+        setResult(eval(calc + value).toString());
+      }
+  }
+
+  const onequals = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const digitsbuttons = () => {
+    const digits = [];
+
+    for(let i=1;i < 10;i++){
+      digits.push(
+        <button key={i} id={i} onClick={() => updateCalc(i.toString())}>{i}</button>
+      );
+    }
+    
+    return digits
+  }
+
+  const clear = () => {
+    setCalc('');
+    setResult('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>{result}</span> : ''}&nbsp;
+          {calc}
+        </div>
+        <div className="operators">
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+
+          <button onClick={clear}>AC</button>
+        </div>
+        <div className="digits">
+          { digitsbuttons()}
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+          <button onClick={onequals}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
